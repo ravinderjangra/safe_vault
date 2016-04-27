088b7454e84fcee60b97acec68d20778527ea773
MaidSafe-QA <qa@maidsafe.net>
2016-04-27 09:30:47 +0000

    Updated documentation.

--- master/safe_vault/vault/sidebar-items.js
+++ master/safe_vault/vault/sidebar-items.js
@@ -1,1 +1,1 @@
-initSidebarItems({"struct":[["RoutingNode","Interface for sending and receiving messages to and from other nodes, in the role of a full routing node."],["Vault","Main struct to hold all personas and Routing instance"]]});
+initSidebarItems({"enum":[["Event","An Event raised by a `Node` or `Client` via its event sender."]],"struct":[["NodeInfo","Info about nodes in the routing table."],["RoutingNode","Interface for sending and receiving messages to and from other nodes, in the role of a full routing node."],["Vault","Main struct to hold all personas and Routing instance"]]});
